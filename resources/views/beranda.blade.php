f50f018b9430b62e4e0acb0a8a063d6d3fddad8e
fauziisa16 <fasaalha@gmail.com>
2022-04-12 20:32:26 +0700

    Update

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" href="fileaset/Logo web.png">
     <link rel="stylesheet" type="text/css" href="property/beranda_styles.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
@@ -15,36 +16,36 @@
 <body>
     @include('partials.navbar') <!--(Navbar) this part moved in partials/navbar-->
     <div class="container-content">
-    <form method="post" action="/beranda" enctype="multipart/form-data">
-      @include('partials.input') <!--(Some parts of input) this part moved in partials/input-->
-      <div class="in_fotoMuseum">
-        <p>Foto Museum<a id="notifnull_Foto">&nbsp;</a></p>
-        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#uploadfoto">Pilih Foto</button>
-        <div class="modal fade" id="uploadfoto" tabindex="-1" aria-labelledby="uploadfotoLabel" aria-hidden="true">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="uploadfotoLabel"><b>Unggah Foto Museum</b></h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body">
-                <label>
-                *Maksimal hanya 1 foto dalam 1 file .jpg/.jpeg/.png<br/>
-                *Ulangi unggah foto untuk merubah file foto</label><br/><br/>
-                <input type="file" class="form-control" id="value_fotoMuseum" name="value_fotoMuseum" onchange="showPreview(event);" accept="image/*"/>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-danger" id="showimage">Simpan</button>
+      <form method="post" action="/beranda" enctype="multipart/form-data">
+        @include('partials.input') <!--(Some parts of input) this part moved in partials/input-->
+        <div class="in_fotoMuseum">
+          <p>Foto Museum<a id="notifnull_Foto">&nbsp;</a></p>
+          <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#uploadfoto">Pilih Foto</button>
+          <div class="modal fade" id="uploadfoto" tabindex="-1" aria-labelledby="uploadfotoLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="uploadfotoLabel"><b>Unggah Foto Museum</b></h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <label>
+                  *Maksimal hanya 1 foto dalam 1 file .jpg/.jpeg/.png<br/>
+                  *Ulangi unggah foto untuk merubah file foto</label><br/><br/>
+                  <input type="file" class="form-control" id="value_fotoMuseum" name="value_fotoMuseum" onchange="showPreview(event);" accept="image/*"/>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-danger" id="showimage">Simpan</button>
+                </div>
               </div>
             </div>
           </div>
+          <img id="image-preview" alt="image preview"/><br/>
+          <ul id="savefrom_errList"></ul>
         </div>
-        <img id="image-preview" alt="image preview"/><br/>
-        <ul id="savefrom_errList"></ul>
-      </div>
-      <br/>
-      <button class="btn btn-danger run" id="button_submit" type="submit" value="Buat Destinasi" style="width: 150px" onclick="ValidateForm()">Buat Destinasi</button>
-    </form>
+        <br/>
+        <button class="btn btn-danger run" id="button_submit" type="submit" value="Buat Destinasi" style="width: 150px" onclick="ValidateForm()">Buat Destinasi</button>
+      </form>
     </div>
 </body>
 
@@ -102,7 +103,7 @@
                   },
                   statusCode: {
                     500: function(){
-                            alert("Error");
+                            alert("Error, silahkan lengkapi input data");
                         }
                   }
               });
--- resources/views/riwayat/edit.blade.php
+++ resources/views/riwayat/edit.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="fileaset/Logo web.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     <script src="{{ asset('assets/jquery.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
